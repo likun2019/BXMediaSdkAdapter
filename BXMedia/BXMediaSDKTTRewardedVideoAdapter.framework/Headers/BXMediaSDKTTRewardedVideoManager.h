d08b72de2ac6358d11990133137229ab6e6bea36
likun <zziajkx@gmail.com>
2021-03-29 17:46:32 +0800

    初始化sdk

---
+++ BXMedia/BXMediaSDKTTRewardedVideoAdapter.framework/Headers/BXMediaSDKTTRewardedVideoManager.h
@@ -0,0 +1,17 @@
+//
+//  BXMediaSDKTTRewardedVideoManager.h
+//  BXMediaSDKTTRewardedVideoAdapter
+//
+//  Created by likun on 2021/3/26.
+//  Copyright © 2021 LIKUN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <BXMediaSDK/BXMediaCenterSDK.h>
+@interface BXMediaSDKTTRewardedVideoManager : NSObject<BXMediaCenterSDKDelegate>
+
+//@property (nonatomic,strong) UIViewController *rootViewController;
+
+@end
+
